cf90fdc14e06b3449f6cbeda09f9f3565d00fcbc
Ing. David Fiedler <fiedlda1@fel.cvut.cz>
2017-08-02 16:34:27 +0200

    EdgeId added

---
+++ src/main/java/cz/cvut/fel/aic/geographtools/EdgeId.java
@@ -0,0 +1,39 @@
+package cz.cvut.fel.aic.geographtools;
+
+/**
+ * @author Marek Cuchý
+ */
+public class EdgeId {
+
+	public final int from;
+	public final int to;
+
+	public EdgeId(int from, int to) {
+		this.from = from;
+		this.to = to;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		EdgeId edgeId = (EdgeId) o;
+
+		return from == edgeId.from && to == edgeId.to;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = from;
+		result = 31 * result + to;
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + from +
+				", " + to +
+				']';
+	}
+}
