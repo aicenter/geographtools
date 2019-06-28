202c435fd00ba9e2f037545769939398316ff88c
Martin Lukeš <lukesma8@fel.cvut.cz>
2019-06-28 15:41:55 +0200

    testMethodComputeGreatCircleDistance modified

--- src/test/java/cz/cvut/fel/aic/geographtools/util/DistanceUtilTest.java
+++ src/test/java/cz/cvut/fel/aic/geographtools/util/DistanceUtilTest.java
@@ -13,17 +13,33 @@ public class DistanceUtilTest {
 
 	@Test
 	public void testMethodComputeGreatCircleDistance() {
+                // distances are from https://www.cqsrg.org/tools/GCDistance/
+                double d1lat, d1lon ,d2lat ,d2lot ,distance;
+                double delta = 0.005; // 0.5 percent
+                
+                d1lat = 50.0882367; d1lon = 14.3448108;
+                d2lat = 50.0762436; d2lot = 14.4189147;
+                
+                distance = 5469.054;		
 
-		double latPetrinyMetro = 50.0882367;
-		double lonPetrinyMetro = 14.3448108;
-		double latKarlakCvut = 50.0762436;
-		double lonKarlakCvut = 14.4189147;
+		Assert.assertEquals(distance, DistanceUtil.computeGreatCircleDistance(d1lat,
+				d1lon, d2lat, d2lot), delta * distance);
+                
+                d1lat = 50.080128; d1lon = 14.395388;
+                d2lat = 50.079301; d2lot = 14.373770;
+                
+                distance = 1550.086;		
 
-		double distanceInMapsCZ = 5452;
-		double delta = 0.01;
+		Assert.assertEquals(distance, DistanceUtil.computeGreatCircleDistance(d1lat,
+				d1lon, d2lat, d2lot), delta * distance);
+                
+                d1lat = 50.075911; d1lon = 14.419055;
+                d2lat = 50.075821; d2lot = 14.413369;
+                
+                distance = 407.143;		
 
-		Assert.assertEquals(distanceInMapsCZ, DistanceUtil.computeGreatCircleDistance(latPetrinyMetro,
-				lonPetrinyMetro, latKarlakCvut, lonKarlakCvut), distanceInMapsCZ * delta);
+		Assert.assertEquals(distance, DistanceUtil.computeGreatCircleDistance(d1lat,
+				d1lon, d2lat, d2lot), delta * distance);
 
 	}
 }
